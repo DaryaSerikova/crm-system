d3950ca0a5e85495846d6c74ca7dc836e85abac6
Daria Serikova <dvserikova@gmail.com>
2025-09-30 22:00:26 +0700

    TodoListPage and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,14 @@
-import { useState, useEffect} from 'react';
-import AddTodo from './components/add-todo/add-todo';
-import TodoList from './components/todo-list/todo-list';
-import Filters from './components/filters/filters';
-import type { IFullTodo, TListsInfo, TFilter } from './types/types';
-import { getAllTodos } from './api/api';
-import s from './App.module.scss';
+import TodoListPage from './pages/TodoListPage/TodoListPage';
+// import s from './App.module.scss';
 
 
 
 function App() {
-  const [todos, setTodos] = useState<IFullTodo[] | null>(null);
-  const [listFilter, setListFilter] = useState<TFilter>("all");
-  const [listsInfo, setListsInfo] = useState<TListsInfo>(null);
-
-  useEffect(() => {
-    if (todos === null) {
-      getAllTodos(listFilter).then(allTodos => {
-        setTodos(allTodos.data);
-        setListsInfo(allTodos.info);
-      });
-    }
-  }, []);
-
-  useEffect(() => {
-    if (todos !== null) {
-      getAllTodos(listFilter).then(allTodos => {
-        setTodos(allTodos.data);
-        setListsInfo(allTodos.info);
-      });
-    }
-    
-  }, [listFilter]);
-
 
   return (
-    <div className={s.app}>
-      <div className={s.card}>
-        <h1 className={s.header}>To do</h1>
-
-        <AddTodo 
-          todos={todos}
-          setTodos={setTodos}
-          listsInfo={listsInfo}
-          setListsInfo={setListsInfo}
-          listFilter={listFilter}
-        />
-
-        <Filters 
-          setListFilter={setListFilter}
-          listsInfo={listsInfo}
-        />
-        { todos && <TodoList 
-          todos={todos} 
-          setTodos={setTodos} 
-          listFilter={listFilter}
-          setListsInfo={setListsInfo}
-          listsInfo={listsInfo}
-          />}
-      </div>
-    </div>
+    <>
+      <TodoListPage />
+    </>
   )
 }
 
---
+++ src/pages/TodoListPage/TodoListPage.tsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react';
+import AddTodo from '../../components/add-todo/add-todo';
+import Filters from '../../components/filters/filters';
+import TodoList from '../../components/todo-list/todo-list';
+import type { IFullTodo, TFilter, TListsInfo } from '../../types/types';
+import { getAllTodos } from '../../api/api';
+import s from './TodoListPage.module.scss';
+
+// interface ITodoListPageProps {}
+
+const TodoListPage = () => {
+// const TodoListPage = (props: ITodoListPageProps) => {
+
+  const [todos, setTodos] = useState<IFullTodo[] | null>(null);
+  const [listFilter, setListFilter] = useState<TFilter>("all");
+  const [listsInfo, setListsInfo] = useState<TListsInfo>(null);
+
+  useEffect(() => {
+    if (todos === null) {
+      getAllTodos(listFilter).then(allTodos => {
+        setTodos(allTodos.data);
+        setListsInfo(allTodos.info);
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (todos !== null) {
+      getAllTodos(listFilter).then(allTodos => {
+        setTodos(allTodos.data);
+        setListsInfo(allTodos.info);
+      });
+    }
+    
+  }, [listFilter]);
+
+
+  return (
+    <div className={s.todoListPage}>
+      <div className={s.card}>
+        <h1 className={s.header}>To do</h1>
+
+        <AddTodo 
+          todos={todos}
+          setTodos={setTodos}
+          listsInfo={listsInfo}
+          setListsInfo={setListsInfo}
+          listFilter={listFilter}
+        />
+
+        <Filters 
+          setListFilter={setListFilter}
+          listsInfo={listsInfo}
+        />
+        { todos && <TodoList 
+          todos={todos} 
+          setTodos={setTodos} 
+          listFilter={listFilter}
+          setListsInfo={setListsInfo}
+          listsInfo={listsInfo}
+          />}
+      </div>
+    </div>
+  )
+}
+
+export default TodoListPage;
