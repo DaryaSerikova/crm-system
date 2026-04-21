b39eb9342c0f0877c91f61ee843135c7809739fa
Daria Serikova <dvserikova@gmail.com>
2026-04-21 21:15:04 +0700

    Layout: LoginPage, RegisterPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,12 +1,14 @@
 import type { AuthData } from '@/types/types';
 import type { FormProps } from 'antd';
 import { Form, Input, Button } from 'antd';
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { openNotification } from '@/utils/errors';
 import { loginUser } from '@/api/api';
 import { useAppDispatch } from '@/store/hooks';
 import { setAuth } from '@/store/slices/authSlice';
 import s from "./LoginPage.module.scss";
+import skelet from '../../assets/images/skelet.png';
+import AuthIcon from '@/assets/icons/authIcon';
 
 
 
@@ -54,63 +56,82 @@ const LoginPage = () => {
 
   return (
     <div className={s.login}>
-      <div className={s.card}>
-        <header className={s.header}>
-          <h1 className={s.h1}>Login to your Account</h1>
-          <p className={s.p}>See what is going on with your business</p>
-        </header>
-        <Form
-          className={s.loginForm}
-          name="login"
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          initialValues={{
-            login: "",
-            password: "",
-          }}
-        >
-          <Item<FieldType>
-            label="Login"
-            name="login"
-            layout="vertical"
-            style={{ flex: 1 }}
-            rules={[
-              { required: true, message: 'Обязательное поле' },
-            ]}
-          >
-            <Input className={s.customInput}
-            />
-          </Item>
-          <Item<FieldType>
-            label="Password"
-            name="password"
-            layout="vertical"
-            style={{ flex: 1 }}
-            rules={[
-              { required: true, message: 'Обязательное поле' },
-            ]}
-          >
-            <Password className={s.customInput} />
-          </Item>
+      <div className={s.imageWrapper}>
+        <img
+          className={s.image}
+          src={skelet}
+          alt='skelet'
+         />
+      </div>
 
-          {/* <Item<FieldType>
-            layout="horizontal"
-            name="rememberMe"
-            valuePropName="checked"
+      
+      <div className={s.cardWrapper}>
+        <div className={s.card}>
+          <header className={s.header}>
+            <AuthIcon />
+            <h1 className={s.h1}>Login to your Account</h1>
+            <p className={s.p}>See what is going on with your business</p>
+          </header>
+          <Form
+            className={s.loginForm}
+            name="login"
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            initialValues={{
+              login: "",
+              password: "",
+            }}
           >
-            <Flex justify="space-between" align="center" style={{ width: '100%' }}>
-              <Checkbox>Remember me</Checkbox>
-              <Link to="#">Forgot Password?</Link>
-            </Flex>
-          </Item> */}
-          <Button 
-            type='primary'
-            className={s.customButton}
-            htmlType='submit'
-          >
-            Login
-          </Button>
-        </Form>
+            <Item<FieldType>
+              label="Login"
+              name="login"
+              layout="vertical"
+              style={{ flex: 1 }}
+              rules={[
+                { required: true, message: 'Обязательное поле' },
+              ]}
+            >
+              <Input className={s.customInput}
+              />
+            </Item>
+            <Item<FieldType>
+              label="Password"
+              name="password"
+              layout="vertical"
+              style={{ flex: 1 }}
+              rules={[
+                { required: true, message: 'Обязательное поле' },
+              ]}
+            >
+              <Password className={s.customInput} />
+            </Item>
+
+            {/* <Item<FieldType>
+              layout="horizontal"
+              name="rememberMe"
+              valuePropName="checked"
+            >
+              <Flex justify="space-between" align="center" style={{ width: '100%' }}>
+                <Checkbox>Remember me</Checkbox>
+                <Link to="#">Forgot Password?</Link>
+              </Flex>
+            </Item> */}
+            <Button 
+              type='primary'
+              className={s.customButton}
+              htmlType='submit'
+            >
+              Login
+            </Button>
+          </Form>
+        </div>
+
+        <div className={s.registerLink}>
+          <p className={s.text}>Not Registered Yet?</p>
+          <Link to='/register' className={s.link}>
+            Create an account
+          </Link>
+        </div>
       </div>
     </div>
   )
--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -6,6 +6,8 @@ import { PhoneInput } from '@/components/ui/phoneInput';
 import { openNotification } from '@/utils/errors';
 import { registerUser } from '@/api/api';
 import s from './RegisterPage.module.scss';
+import skelet from '../../assets/images/skelet.png';
+import AuthIcon from '@/assets/icons/authIcon';
 
 
 const RegisterPage = () => {
@@ -59,122 +61,132 @@ const RegisterPage = () => {
 
   return (
     <div className={s.register}>
-      <div className={s.card}>
-        <header className={s.header}>
-          <h1 className={s.h1}>Register to your Account</h1>
-          <p className={s.p}>See what is going on with your business</p>
-        </header>
+      <div className={s.imageWrapper}>
+        <img
+          className={s.image}
+          src={skelet}
+          alt='skelet'
+         />
+      </div>
+      <div className={s.cardWrapper}>
+        <div className={s.card}>
+          <header className={s.header}>
+            <AuthIcon />
+            <h1 className={s.h1}>Register to your Account</h1>
+            <p className={s.p}>See what is going on with your business</p>
+          </header>
 
-        <Form
-          className={s.registerForm}
-          name="register"
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-        >
-          <Item<FieldType>
-            label="Имя пользователя"
-            name="username"
-            layout="vertical"
-            rules={[
-              { required: true, message: 'Обязательное поле' },
-              { min: 1, message: 'Cимволов не может быть менее 1' },
-              { max: 60, message: 'Cимволов не может быть более 60' },
-              { 
-                pattern: /^[a-zA-Zа-яА-ЯёЁ ]+$/, 
-                message: 'Имя может содержать только символы русского/латинского алфавита'
-              }
-            ]}
-            style={{ flex: 1 }}
+          <Form
+            className={s.registerForm}
+            name="register"
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
           >
-            <Input className={s.customInput} />
-          </Item>
-          <Item<FieldType>
-            label="Логин"
-            name="login"
-            layout="vertical"
-            rules={[
-              { required: true, message: 'Обязательное поле' },
-              { min: 2, message: 'Cимволов не может быть менее 2' },
-              { max: 60, message: 'Cимволов не может быть более 60' },
-              { 
-                pattern: /^[a-zA-Z ]+$/, 
-                message: 'Логин может содержать только символы латинского алфавита'
-              }
-            ]}
-            style={{ flex: 1 }}
-          >
-            <Input className={s.customInput} />
-          </Item>
-          <Item<FieldType>
-            label="Пароль"
-            name="password"
-            layout="vertical"
-            rules={[
-              { required: true, message: 'Обязательное поле' },
-              { min: 6, message: 'Cимволов не может быть менее 6' },
-              { max: 60, message: 'Cимволов не может быть более 60' },
-            ]}
-            style={{ flex: 1 }}
-          >
-            <Password className={s.customInput} />
-          </Item>
-          <Item<FieldType>
-            label="Повторите пароль"
-            name="password2"
-            layout="vertical"
-            dependencies={['password']}
-            rules={[
-              { required: true, message: 'Поле не может быть пустым!!' },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue('password') === value) {
-                    return Promise.resolve();
-                  }
-                  return Promise.reject(new Error('Пароли не совпадают!'));
-                },
-              }),
-            ]}
-            style={{ flex: 1 }}
-          >
-            <Password className={s.customInput} />
-          </Item>
-          <Item<FieldType>
-            label="Почтовый адрес"
-            name="email"
-            layout="vertical"
-            rules={[
-              { required: true, message: 'Обязательное поле' },
-              { 
-                pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 
-                message: 'Невалидный email'
-              }
-            ]}
-            style={{ flex: 1 }}
-          >
-            <Input className={s.customInput} />
-          </Item>
-          <Item<FieldType>
+            <Item<FieldType>
+              label="Имя пользователя"
+              name="username"
               layout="vertical"
-              label="Телефон"
-              name="phone"
-              style={{flex: 1}}
+              rules={[
+                { required: true, message: 'Обязательное поле' },
+                { min: 1, message: 'Cимволов не может быть менее 1' },
+                { max: 60, message: 'Cимволов не может быть более 60' },
+                { 
+                  pattern: /^[a-zA-Zа-яА-ЯёЁ ]+$/, 
+                  message: 'Имя может содержать только символы русского/латинского алфавита'
+                }
+              ]}
+              style={{ flex: 1 }}
             >
-            <PhoneInput />
+              <Input className={s.customInput} />
             </Item>
-          <Button 
-            type="primary" 
-            className={s.customButton} 
-            htmlType="submit"
-          >
-            Register
-          </Button>
-        </Form>
-        { isOpenLink 
-          ? <Link to={"/login"}>
-              Перейти на страницу авторизации для входа в систему
-            </Link>
-          : <></>}
+            <Item<FieldType>
+              label="Логин"
+              name="login"
+              layout="vertical"
+              rules={[
+                { required: true, message: 'Обязательное поле' },
+                { min: 2, message: 'Cимволов не может быть менее 2' },
+                { max: 60, message: 'Cимволов не может быть более 60' },
+                { 
+                  pattern: /^[a-zA-Z ]+$/, 
+                  message: 'Логин может содержать только символы латинского алфавита'
+                }
+              ]}
+              style={{ flex: 1 }}
+            >
+              <Input className={s.customInput} />
+            </Item>
+            <Item<FieldType>
+              label="Пароль"
+              name="password"
+              layout="vertical"
+              rules={[
+                { required: true, message: 'Обязательное поле' },
+                { min: 6, message: 'Cимволов не может быть менее 6' },
+                { max: 60, message: 'Cимволов не может быть более 60' },
+              ]}
+              style={{ flex: 1 }}
+            >
+              <Password className={s.customInput} />
+            </Item>
+            <Item<FieldType>
+              label="Повторите пароль"
+              name="password2"
+              layout="vertical"
+              dependencies={['password']}
+              rules={[
+                { required: true, message: 'Поле не может быть пустым!!' },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue('password') === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(new Error('Пароли не совпадают!'));
+                  },
+                }),
+              ]}
+              style={{ flex: 1 }}
+            >
+              <Password className={s.customInput} />
+            </Item>
+            <Item<FieldType>
+              label="Почтовый адрес"
+              name="email"
+              layout="vertical"
+              rules={[
+                { required: true, message: 'Обязательное поле' },
+                { 
+                  pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 
+                  message: 'Невалидный email'
+                }
+              ]}
+              style={{ flex: 1 }}
+            >
+              <Input className={s.customInput} />
+            </Item>
+            <Item<FieldType>
+                layout="vertical"
+                label="Телефон"
+                name="phone"
+                style={{flex: 1}}
+              >
+              <PhoneInput />
+              </Item>
+            <Button 
+              type="primary" 
+              className={s.customButton} 
+              htmlType="submit"
+            >
+              Register
+            </Button>
+          </Form>
+          { isOpenLink 
+            ? <Link to={"/login"}>
+                Перейти на страницу авторизации для входа в систему
+              </Link>
+            : <></>}
 
+        </div>
       </div>
     </div>
   )
